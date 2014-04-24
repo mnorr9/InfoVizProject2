f0bc91ae83b2038827acd262ff36c5aaf1291a83
nabreu <nabreu@mse-f10ydx1.njmrsnet-aegis.com>
2014-04-24 10:45:02 -0400

    Rename Project2.java (the JFrame) to something more descriptive.

--- src/project2/AppLauncher.java
+++ src/project2/AppLauncher.java
@@ -22,7 +22,7 @@ public class AppLauncher {
     
        public static void main(String[] args) {
         @SuppressWarnings("unused")
-        Project2 proj = new Project2();
+        MainForm proj = new MainForm();
         Visualization scene = new Visualization();
         scene.attachToPanel(proj.pnlPrimary);
         
