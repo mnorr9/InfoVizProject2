d4088aac0b75fbba9ebd21a54f11bd0f10229ebe
David <David@10.0.0.72>
2014-04-28 22:02:33 -0400

    added comments

--- src/database/Kicker.java
+++ src/database/Kicker.java
@@ -13,7 +13,12 @@ public class Kicker extends Player
   private double extra_point_percentage;
   private double extra_point_points;
   private static final String splitBy = "-";
-  
+
+  /**
+   *   Constructor for the Kicker class.
+   *   Creates instances of the field goal stats class
+   *   for attempts and makes.
+   */
   public Kicker()
   {
     distance_attempted = new FGDistanceStats();
@@ -23,7 +28,14 @@ public class Kicker extends Player
     fumbles      = 0;
     fumbles_lost = 0;
   }
-  
+
+  /**
+   *   Function to populate the fields in the kicker class.
+   *   Takes an array string that contains each element in
+   *   the class.
+   *   
+   *   @param stat_field
+   */
   public void createKicker(String[] stat_field)
   {
     String[] temp;
@@ -56,89 +68,195 @@ public class Kicker extends Player
     extra_point_points            = Double.parseDouble(stat_field[15]);
   }
 
+  /**
+   *   Getter to retrieve the number of field goals made
+   *   between 10 and 19 yards.
+   *    
+   *   @return double
+   */
   public double get10To19FieldGoalsMade()
   {
     return distance_made.get10To19FieldGoals();
   }
   
+  /**
+   *   Getter to retrieve the number of field goal attempts
+   *   between 10 and 19 yards.
+   *    
+   *   @return double
+   */
   public double get10To19FieldGoalsAttempted()
   {
     return distance_attempted.get10To19FieldGoals();
   }
   
+  /**
+   *   Getter to retrieve the number of field goals made
+   *   between 20 and 29 yards.
+   *    
+   *   @return double
+   */
   public double get20To29FieldGoalsMade()
   {
     return distance_made.get20To29FieldGoals();
   }
   
+  /**
+   *   Getter to retrieve the number of field goal attempts
+   *   between 20 and 29 yards.
+   *    
+   *   @return double
+   */
   public double get20To29FieldGoalsAttempted()
   {
     return distance_attempted.get20To29FieldGoals();
   }
   
+  /**
+   *   Getter to retrieve the number of field goals made
+   *   between 30 and 39 yards.
+   *    
+   *   @return double
+   */
   public double get30To39FieldGoalsMade()
   {
     return distance_made.get30To39FieldGoals();
   }
   
+  /**
+   *   Getter to retrieve the number of field goal attempts
+   *   between 30 and 39 yards.
+   *    
+   *   @return double
+   */
   public double get30To39FieldGoalsAttempted()
   {
     return distance_attempted.get30To39FieldGoals();
   }
   
+  /**
+   *   Getter to retrieve the number of field goals made
+   *   between 40 and 49 yards.
+   *    
+   *   @return double
+   */
   public double get40To49FieldGoalsMade()
   {
     return distance_made.get40To49FieldGoals();
   }
   
+  /**
+   *   Getter to retrieve the number of field goal attempts
+   *   between 40 and 49 yards.
+   *    
+   *   @return double
+   */
   public double get40To49FieldGoalsAttempted()
   {
     return distance_attempted.get40To49FieldGoals();
   }
   
+  /**
+   *   Getter to retrieve the number of field goals made
+   *   from 50+ yards.
+   *    
+   *   @return double
+   */
   public double get50PlusFieldGoalsMade()
   {
     return distance_made.get50PlusFieldGoals();
   }
   
+  /**
+   *   Getter to retrieve the number of field goal attempts
+   *   from 50+ yards.
+   *    
+   *   @return double
+   */
   public double get50PlusFieldGoalsAttempted()
   {
     return distance_attempted.get50PlusFieldGoals();
   }
 
+  /**
+   *   Getter to retrieve the number of field goals made.
+   *
+   *   @return double
+   */
   public double getFieldGoalsMade()
   {
     return field_goals_made;
   }
 
+  /**
+   *   Getter to retrieve the number of field goal attempts.
+   *
+   *   @return double
+   */
   public double getFieldGoalAttempts()
   {
     return field_goal_attempts;
   }
 
+  /**
+   *   Getter to retrieve the kickers field goal percentage.
+   *
+   *   @return double
+   */
   public double getFieldGoalPercentage()
   {
     return field_goal_percentage;
   }
   
+  /**
+   *   Getter to retrieve the kickers longest field
+   *   goal made.
+   *
+   *   @return double
+   */
   public double getLongestFieldGoal()
   {
     return field_goal_long;
   }
 
+  /**
+   *   Getter to retrieve the number of extra points made.
+   *
+   *   @return double
+   */
   public double getExtraPointsMade()
   {
     return extra_points_made;
   }
 
+  /**
+   *   Getter to retrieve the number of extra point attempts.
+   *
+   *   @return double
+   */
   public double getExtraPointAttempts()
   {
     return extra_point_attempts;
   }
 
+  /**
+   *   Getter to retrieve the kickers extra point percentage.
+   *
+   *   @return double
+   */
   public double getExtraPointPercentage()
   {
     return extra_point_percentage;
   }
 
+  /**
+   *   Getter to retrieve the kickers extra point points.
+   *
+   *   @return double
+   */
+  public double getExtraPointPoints()
+  {
+    return extra_point_points;
+  }
+  
 }
